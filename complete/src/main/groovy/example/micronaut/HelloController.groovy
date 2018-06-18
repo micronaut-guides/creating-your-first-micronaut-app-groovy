db0f020a3dd62f9b7b67685ac29bd81f47b00d77
Pablo Alba <pablo.alba@kaleidos.net>
2018-06-18 15:56:28 +0200

    Add content type text plain

--- complete/src/main/groovy/example/micronaut/HelloController.groovy
+++ complete/src/main/groovy/example/micronaut/HelloController.groovy
@@ -3,11 +3,14 @@ package example.micronaut
 import groovy.transform.CompileStatic
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Produces
+import io.micronaut.http.MediaType
 
 @CompileStatic
 @Controller("/hello") // <1>
 class HelloController {
     @Get("/") // <2>
+    @Produces(MediaType.TEXT_PLAIN)
     String index() {
         "Hello World" // <3>
     }
